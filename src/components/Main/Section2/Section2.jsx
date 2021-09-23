40efb4a8ded90f616e9a3c801b968fec3ed8524f
kuldeep230 <ramdeep214@gmail.com>
2021-09-23 15:03:38 +0530

    minor change in Section1 and Section3 is partially completed

---
+++ src/components/Main/Section2/Section2.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Section2 = () => {
+  return (
+    <div>
+      <h1>Hold on Section2</h1>
+    </div>
+  );
+};
+export default Section2;
---
+++ src/components/Main/Section3/Section3.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+//CSS
+import "./Section3.css";
+//Images
+import IllustrationLaptop from "../../../assets/images/illustration-laptop-mobile.svg";
+
+const Section3 = () => {
+  return (
+    <div className="section section3">
+      <div className="content-wrapper">
+        <img
+          className="img-illustrationLaptop"
+          src={IllustrationLaptop}
+          alt="illustration laptop"
+        />
+        <div className="text-block">
+          <h3>Free, open, simple</h3>
+          <p>
+            Blogr is a free and open source application backed by a large
+            community of helpful developers. It supports features such as code
+            syntax highlighting. RSS feeds, social media integration,
+            third-party commenting tools, and works seamlessly with Google
+            Analytics. The architecture is clean and is relatively easy to
+            learn.
+          </p>
+          <h3>Powerful tooling</h3>
+          <p>
+            Batteries included. We built a simple and straightforward CLI tool
+            that makes customization and deployment a breeze, but capable of
+            producing even the most complicated sites.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Section3;
